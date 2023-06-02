c9589086fba2d8645134f9ff104b618d335f1352
高旭东 <gaoxudong.xudong@bytedance.com>
2023-06-02 17:12:47 +0800

    Initial Commit

---
+++ MyXCode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyXCode
+//
+//  Created by GaoXudong on 2023/6/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
